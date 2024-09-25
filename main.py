0f8637d85872fe256fce1e55810c97205619abec
Jessica Mary <jessicamary472@gmail.com>
2024-09-25 18:34:25 +0800

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://maryjess.github.io/jessica-mary/'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+data = soup.find_all()
+
+with open('data.txt', 'w') as file:
+	for item in data:
+		file.write(item.text + '\n')
