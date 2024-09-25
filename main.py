5058f8a6ade7397049e4a66d01a231f4d02605ad
Jessica Mary <jessicamary472@gmail.com>
2024-09-25 21:09:02 +0800

    Solved #1

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
-from bs4 import BeautifulSoup # got imports issue, better refactor to setup.py
+from bs4 import BeautifulSoup
 
 url = 'https://maryjess.github.io/jessica-mary/'
-response = requests.get(url)
+html_text = requests.get(url).json()["results"][0]["content"]
+soup = BeautifulSoup(html_text, "html.parser")
 
-soup = BeautifulSoup(response.content, 'html.parser')
 data = soup.find_all()
 
 with open('data.txt', 'w') as file:
 	for item in data:
-		file.write(item.text + '\n')
+		file.write(soup.get_text(strip=True, separator=" "))
